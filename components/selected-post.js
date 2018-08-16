86686268a649a0d2a7eb3a231bdb078cc0df48c2
fajartc02 <fajartc02@gmail.com>
2018-08-16 13:24:57 +0700

    selected-post temporary

--- components/selected-post.js
+++ components/selected-post.js
@@ -1,25 +1,22 @@
 Vue.component('selected-post', {
   template: 
   `
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal Title
-            </TItle>
-          </h5>
-          <button type="button" class="close" data-dismiss="modal">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body row">
-          <div class="col-md-4">
-          <img :src="image" style="height:200px;width:200px"></img>
+  <div class="modal fade" id="test-modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Modal Title
+            </h5>
+            <button type="button" class="close" data-dismiss="modal">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-          <p>This is modal body</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary" data-dismiss="modal">Share</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <div class="modal-body">
+            <p>This is modal body</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
         </div>
       </div>
     </div>
@@ -32,8 +29,5 @@ Vue.component('selected-post', {
   methods: {
 
   },
-  props: ['image'],
-  created() {
-
-  },
+  props: ['images']
 })
