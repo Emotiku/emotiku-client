62fc415fb6b4c2bc6964829953e516b480bee280
fajartc02 <fajartc02@gmail.com>
2018-08-16 15:36:07 +0700

    client done

--- components/selected-post.js
+++ components/selected-post.js
@@ -4,7 +4,7 @@ Vue.component('selected-post', {
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal Title
+          <h5 class="modal-title">{{image.emotion}}
           </h5>
           <button type="button" class="close" data-dismiss="modal">
             <span aria-hidden="true">&times;</span>
@@ -12,13 +12,11 @@ Vue.component('selected-post', {
         </div>
         <div class="modal-body">
           <img :src="image.url"></img>
-          <p>This is modal body</p>
         </div>
         <div class="modal-footer">
           <div class="fb-share-button" :data-href="image" data-layout="button" data-size="large" data-mobile-iframe="false"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Bagikan</a></div>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="downloadFile(image)"><i class="fas fa-cloud-download-alt"></i> Download</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="downloadFile(image.url)"><i class="fas fa-cloud-download-alt"></i> Download</button>
           <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="removedFile(image._id)"><i class="fas fa-trash-alt"></i> Delete</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-window-close"></i> Close</button>
         </div>
       </div>
     </div>
@@ -49,9 +47,9 @@ Vue.component('selected-post', {
       })
     },
     removedFile: function(id) {
-      axios.delete(`http://35.240.209.66/images/5b75288fc72c370c77e2a26d`, {
+      axios.delete(`http://35.240.209.66/images/${id}`, {
         headers: {
-          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViNzQzNWQ3NDZhZWJmMGY5OTcyNmJmMCIsIm5hbWUiOiJvcSIsImVtYWlsIjoib3FAbWFpbC5jb20iLCJpYXQiOjE1MzQ0MDU3Mzl9.IJumL3qkDpZ3wBhcFbagLheIEfMh3nWeUMK7fbFXwBw'
+          token: localStorage.getItem('token')
         } 
       })
       .then(result => {
