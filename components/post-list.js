a5c1b2f88b2d707827f43f1cf2d34826e470beb7
Grayfield <grayfieldmm@outlook.com>
2018-08-16 09:44:03 +0700

    Make the layout responsive

--- components/post-list.js
+++ components/post-list.js
@@ -1,21 +1,16 @@
 Vue.component('post-list', {
   template:
     `
-      <div>
-          
-            <div class="card-deck">
-              <div v-for="post in posts" class="card mb-3">
-                <img class="card-img-top" :src="post.url" alt="image alt">
-                <div class="card-body">
-                  <h5 class="card-title">{{ post.emotion }}</h5>
-                  <p>
-                    <button class="btn btn-primary" data-toggle="modal" data-target="#test-modal">
-                      More
-                    </button>
-                  </p>
-                </div>
-              </div>
-            </div>
+      <div class="card-deck">
+        <div v-for="post in posts" class="card mb-3">
+          <div class="card-img-container">
+            <img class="card-img-top" :src="post.url" alt="image alt">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{{ post.emotion }}</h5>
+            <p class="card-text card-date"><span class="text-muted text-sm">{{ formatDate(post.createdAt) }}</span></p>
+          </div>
+        </div>
       </div>
     `,
   data() {
@@ -25,6 +20,25 @@ Vue.component('post-list', {
   },
   methods: {
 
+  },
+  computed: {
+    formatDate() {
+      return dateStr => {
+        var date = new Date(dateStr)
+        var monthNames = [
+          "January", "February", "March",
+          "April", "May", "June", "July",
+          "August", "September", "October",
+          "November", "December"
+        ]
+      
+        var day = date.getDate()
+        var monthIndex = date.getMonth()
+        var year = date.getFullYear()
+      
+        return `${monthNames[monthIndex]} ${day}, ${year}`
+      }
+    } 
   },
   props: ['posts']
 })
